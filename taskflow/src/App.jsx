5196a40cbac93f78055b35b6690f1cf8d8c3a5aa
laviniaacioly-cloud <lavinia.acioly@aluno.senai.br>
2026-08-04 17:17:50 -0300

    kanban-final

--- taskflow/src/App.jsx
+++ taskflow/src/App.jsx
@@ -1,13 +1,33 @@
 import "./App.css";
 import Header from "./componentes/Header";
-import ListaTarefas from "./componentes/ListaTarefas";
-import { useState } from "react";
+// import ListaTarefas from "./componentes/ListaTarefas";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [tarefas, setTarefas] = useState([]);
-  const [proximoId, setProximoId] = useState(1);
+  const [tarefas, setTarefas] = useState(() => {
+    //procura as tarefas salvas no navegador
+    const salvo = localStorage.getItem("TaskFlow.tarefas");
+    //transforma o  LocalStorage em array
+    return salvo ? JSON.parse(salvo) : []; // se não existir começa com lista vazia
+  });
+
+  const [proximoId, setProximoId] = useState(() => {
+    const salvo = localStorage.getItem("TaskFlow.tarefas");
+
+    if (salvo) {
+      const dados = JSON.parse(salvo);
+      return dados.length > 0 ? dados[dados.length - 1].id + 1 : 1;
+    }
+
+    return 1;
+  });
+
   const [texto, setTexto] = useState("");
   const [prioridade, setPrioridade] = useState("media");
+
+  useEffect(() => {
+    localStorage.setItem("TaskFlow.tarefas", JSON.stringify(tarefas));
+  }, [tarefas]);
 
   function adicionarTarefa() {
     if (texto.trim() === "") return;
@@ -17,6 +37,7 @@ function App() {
       texto: texto.trim(),
       concluida: false,
       prioridade: prioridade,
+      coluna: "afazer",
     };
 
     setTarefas([...tarefas, nova]);
@@ -25,28 +46,31 @@ function App() {
     setPrioridade("media");
   }
 
+  function moverTarefa(id, novaColuna) {
+    setTarefas(
+      tarefas.map((tarefa) =>
+        tarefa.id === id ? { ...tarefa, coluna: novaColuna } : tarefa,
+      ),
+    );
+  }
   function deletarTarefa(id) {
     setTarefas(tarefas.filter((tarefa) => tarefa.id !== id));
   }
 
-  function concluirTarefa(id) {
-    setTarefas(
-      tarefas.map((tarefa) =>
-        tarefa.id === id
-          ? { ...tarefa, concluida: !tarefa.concluida }
-          : tarefa
-      )
-    );
-  }
+  // function concluirTarefa(id) {
+  //   setTarefas(
+  //     tarefas.map((tarefa) =>
+  //       tarefa.id === id ? { ...tarefa, concluida: !tarefa.concluida } : tarefa,
+  //     ),
+  //   );
+  // }
 
   return (
-    <div className="conteiner" id="app">
-      <Header
-        titulo="TaskFlow"
-        subtitulo="Gerencie suas tarefas"
-      />
+    <div className="conteiner">
+      <Header titulo="TaskFlow" subtitulo="Gerencie suas tarefas" />
 
-      <main className="conteiner">
+      <main>
+        {/* Formulário */}
         <section id="formulario">
           <input
             id="input-tarefa"
@@ -77,41 +101,73 @@ function App() {
           </button>
         </section>
 
-        <section id="controles">
-          <div id="filtros">
-            <button className="btn-filtro ativo">
-              Todas
-            </button>
+        {/* Kanban */}
 
-            <button className="btn-filtro">
-              Pendentes
-            </button>
+        <section className="kanban">
+          <section className="coluna">
+            <h2>A Fazer</h2>
+  {/* fazer essa coluna funcionar */}
+            {tarefas
+              .filter((tarefa) => tarefa.coluna === "afazer")
+              .map((tarefa) => (
+                <div key={tarefa.id} className="card">
+                  <p>{tarefa.texto}</p>
+                   <button onClick={() => moverTarefa(tarefa.id, "andamento")}>
+                      →
+                   </button>
 
-            <button className="btn-filtro">
-              Concluídas
-            </button>
-          </div>
+                   <button onClick={() => deletarTarefa(tarefa.id)}>
+                      🗑️
+                   </button>
+                </div>
+              ))}
+          </section>
 
-          <div id="contadores">
-            <span id="cont-total">
-              {tarefas.length} tarefas
-            </span>
+          <section className="coluna">
+            <h2>Em Andamento</h2>
+  {/* fazer essa coluna funcionar */}
+            {tarefas
+            .filter((tarefa) => tarefa.coluna === "andamento")
+            .map((tarefa) => (
+              <div key={tarefa.id} className="card">
+                <p>{tarefa.texto}</p>
+{/* mover as tarefas */}
+{/* esquerda */}
+                <button onClick={() => moverTarefa(tarefa.id, "afazer")}>
+                  ← 
+                </button>
+{/* direita */}
+                <button onClick={() => moverTarefa(tarefa.id, "concluido")}>
+                  →
+                </button>
 
-            <span id="cont-pendentes">
-              {tarefas.filter((t) => !t.concluida).length} pendentes
-            </span>
+                  <button onClick={() => deletarTarefa(tarefa.id)}>
+                      🗑️
+                  </button>
+              </div>
+            ))}
+          </section>
 
-            <span id="cont-concluidas">
-              {tarefas.filter((t) => t.concluida).length} concluídas
-            </span>
-          </div>
-        </section>
+          <section className="coluna">
+            <h2>Concluído</h2>
+            
+            {tarefas
+              .filter((tarefa) => tarefa.coluna === "concluido")
+              .map((tarefa) => (
+                <div key={tarefa.id} className="card">
+                  <p>{tarefa.texto}</p>
 
-        <ListaTarefas
-          tarefas={tarefas}
-          onDeletar={deletarTarefa}
-          onConcluir={concluirTarefa}
-        />
+                  <button onClick={() => moverTarefa(tarefa.id, "andamento")}>
+                    ←
+                  </button>
+
+                    <button onClick={() => deletarTarefa(tarefa.id)}>
+                      🗑️
+                    </button>
+                </div>
+              ))}
+          </section>
+          </section>
       </main>
 
       <footer>
